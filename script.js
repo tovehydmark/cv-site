0bd20b1b0ec729cdfeaad5bbdf71f90eadc1a537
tovehydmark <tovehydmark@gmail.com>
2022-01-06 10:48:38 +0100

    fetch api from github, prints links on web page, styling

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const githubReposBtn = document.getElementById("githubReposBtn");
 const viewMyPortfolioContainer = document.getElementById("viewMyGithubRepos");
 
+const githubReposSection = document.createElement("section");
+githubReposSection.setAttribute("id", "githubReposSection");
+
 githubReposBtn.addEventListener("click", function () {
 
-    const githubReposSection = document.createElement("section");
-    githubReposSection.setAttribute("id", "githubReposSection");
-
     viewMyPortfolioContainer.insertAdjacentElement("afterend", githubReposSection)
+    githubReposSection.innerHTML = ""
+
+    function fetchRepos() {
+
+        fetch("https://api.github.com/users/tovehydmark/repos")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+
+                console.log("github API", json)
+
+                for (i = 0; i < json.length; i++) {
+
+                    githubReposSection.insertAdjacentHTML("beforeend", "<a href='" + json[i].html_url + "'target='_blank'><div class='githubRepo'>" + json[i].name + "</div")
+                }
+            });
+    }
+    fetchRepos()
 })
